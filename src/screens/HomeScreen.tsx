c2e0c3b32ea1317132d32df383ef3d97b2c6e0d8
chaeheetae <chaeheetae94@gmail.com>
2024-08-19 23:14:12 +0900

    장소 허가 훅 구현

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Text } from "react-native";
+import useLocationPermission from "../hooks/useLocationPermission";
 
 function HomeScreen() {
+  const location = useLocationPermission();
+
   return <Text>홈</Text>;
 }
 
